d934ff7b91bc5014a6e697aecfe21b1756041c2a
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-02 15:36:05 +0200

    day01

--- src/day01.zig
+++ src/day01.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const math = @import("math");
 const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
 const Map = std.AutoHashMap;
@@ -10,8 +11,31 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day01.txt");
 
+fn lessThan(context: void, a: i32, b: i32) std.math.Order { _ = context; return std.math.order(a, b); }
+
 pub fn main() !void {
+    std.debug.print("Day 01\n", .{});
+    var elves = std.PriorityQueue(i32, void, lessThan).init(gpa, {});
+    defer elves.deinit();
     
+    var current_elf: i32 = 0;
+    
+    var lines = split(u8, data, "\n");
+    while (lines.next()) |line| {
+        if(line.len == 0) {
+            try elves.add(-current_elf); // NOTE (Al): adding the values negated because I don't know hoe to make a comparator function to sort them the other way :derp:
+            current_elf = 0;
+            continue;
+        }
+        current_elf += try std.fmt.parseInt(i32, line, 10);
+    }
+    try elves.add(-current_elf);
+
+    var top1 = elves.remove();
+    var top3 = top1 + elves.remove() + elves.remove();
+
+    std.debug.print("Part 1: {d}\n", .{top1});
+    std.debug.print("Part 2: {d}\n", .{top3});
 }
 
 // Useful stdlib functions
